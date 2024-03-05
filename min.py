3c32d80fffecf8a8d5ba512e9cb50561589b74c4
fkjj14 <162050791+fkjj14@users.noreply.github.com>
2024-03-05 20:18:23 +0000

    add to main.py

--- min.py
+++ min.py
@@ -1,7 +1,15 @@
-a = 512*8
-b = 2*2**10
+print("Hello, how are you?")
+user_mood = input()
+print(f"Great, that you are {user_mood}! What is your name?")
+user_name = input()
+print(f"Hello, {user_name}! How old are you?")
+user_age = int(input())
+birth_year_guess = 2024 - user_age
 
-if a > b:
-    print(“1) min of a and a =“, b)
+print(f"I see, you were born in year {birth_year_guess}? Let me know if it is True or False?")
+birth_year_check = input()
+
+if birth_year_check.lower() == "true":
+    print(f"Great, then {birth_year_guess} is your birth year!")
 else:
-    print(“2) min of a and a“, b)
+    print(" Double-check your information.")
